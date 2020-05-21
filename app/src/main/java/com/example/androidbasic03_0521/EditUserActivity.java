d0006da0504188e79925c6b06936aea60f445e32
kimhaneui <kimhaneui2@naver.com>
2020-05-21 15:31:33 +0900

    화면구성 및 닉네임 액티비티 추가

---
+++ app/src/main/java/com/example/androidbasic03_0521/EditUserActivity.java
@@ -0,0 +1,24 @@
+package com.example.androidbasic03_0521;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditUserActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user);
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
