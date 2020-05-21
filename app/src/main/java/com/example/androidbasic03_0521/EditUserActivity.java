4fd236cda10a23152c753da18e91a16f5cd5f5ea
kimhaneui <kimhaneui2@naver.com>
2020-05-21 16:01:13 +0900

    기능 추가

--- app/src/main/java/com/example/androidbasic03_0521/EditUserActivity.java
+++ app/src/main/java/com/example/androidbasic03_0521/EditUserActivity.java
@@ -1,19 +1,42 @@
 package com.example.androidbasic03_0521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.androidbasic03_0521.databinding.ActivityEditUserBinding;
 
 public class EditUserActivity extends BaseActivity {
+
+    ActivityEditUserBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_user);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_edit_user);
+        setupEvents();
+        setValues();
     }
 
     @Override
     public void setupEvents() {
+
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nickName = binding.nickNameEdt.getText().toString();
+
+                Intent myintent = new Intent();
+                myintent.putExtra("editNick",nickName);
+
+                setResult(RESULT_OK,myintent);
+
+                finish();
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/androidbasic03_0521/MainActivity.java
+++ app/src/main/java/com/example/androidbasic03_0521/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.androidbasic03_0521;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+
+import com.example.androidbasic03_0521.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
 
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
         setupEvents();
         setValues();
     }
@@ -18,10 +26,33 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        binding.nickNameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myintent = new Intent(mContext,EditUserActivity.class);
+
+//                결과를 달라
+                startActivityForResult(myintent,1001);
+            }
+        });
+
     }
 
     @Override
     public void setValues() {
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1001){
+            if(resultCode == RESULT_OK){
+                String nick = data.getStringExtra("editNickName");
+                binding.userNickNameTxt.setText(nick);
+
+            }
+        }
     }
 }
