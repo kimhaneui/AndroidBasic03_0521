eb39d57f404f4d84784bf19c97ccb9238697dae2
kimhaneui <kimhaneui2@naver.com>
2020-05-21 16:07:17 +0900

    전화번호 화면 그리기

---
+++ app/src/main/java/com/example/androidbasic03_0521/UserPhoneActivity.java
@@ -0,0 +1,24 @@
+package com.example.androidbasic03_0521;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserPhoneActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_phone2);
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
