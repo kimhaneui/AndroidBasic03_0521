4049646519aaf6248d6198a124f4ba6d005b7c74
kimhaneui <kimhaneui2@naver.com>
2020-05-21 16:33:30 +0900

    변경하기 버튼 누르면 입력한 폰번 추출

--- app/src/main/java/com/example/androidbasic03_0521/UserPhoneActivity.java
+++ app/src/main/java/com/example/androidbasic03_0521/UserPhoneActivity.java
@@ -1,21 +1,36 @@
 package com.example.androidbasic03_0521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.androidbasic03_0521.databinding.ActivityUserPhone2Binding;
 
 public class UserPhoneActivity extends BaseActivity {
+
+    ActivityUserPhone2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_phone2);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_user_phone2);
+        setupEvents();
+        setValues();
     }
 
     @Override
     public void setupEvents() {
 
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputPhoneNum = binding.phoneNumEdt.getText().toString();
+            }
+        });
     }
+
 
     @Override
     public void setValues() {
