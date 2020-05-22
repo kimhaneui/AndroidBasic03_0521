f7600896ab970b1d3a5831647bb45c214e704fd3
kimhaneui <kimhaneui2@naver.com>
2020-05-22 09:43:56 +0900

    카카오톡 링크 연결하기

--- app/src/main/java/com/example/androidbasic03_0521/MainActivity.java
+++ app/src/main/java/com/example/androidbasic03_0521/MainActivity.java
@@ -32,6 +32,16 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        binding.storeLinkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                플레이스토어에서 원하는 앱의 링크 주소를 detail 부터 복붙
+//                웹상에서 검색 가능함
+                Uri myuri = Uri.parse("market://details?id=com.kakao.talk");
+                Intent myintent = new Intent(Intent.ACTION_VIEW, myuri);
+                startActivity(myintent);
+            }
+        });
         binding.webLinkBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -129,6 +139,7 @@ public class MainActivity extends BaseActivity {
                 if(data != null){
                        String phoneNum = data.getStringExtra("phone");
                        binding.userPhoneTxt.setText(phoneNum);
+
                 }
             }
             else{
